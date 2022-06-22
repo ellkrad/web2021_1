77d0b790d8f600cf05bc664c5b6f6f838388486a
kel orz <86697562+ellkrad@users.noreply.github.com>
2022-06-22 06:11:30 +0000

    carテーブルの構造を定義

--- create.js
+++ create.js
@@ -1,10 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
+//let schema = `
+//create table test(
+//  id integer primary key,
+//  name text
+//);
+//`
+
 let schema = `
 create table test(
-  id integer primary key,
-  name text
+id integer primary key,
+name text not null,
+maker_id integer not null
 );
 `
 
