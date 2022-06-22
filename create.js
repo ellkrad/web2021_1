782f1148a7b13cb150266413ecda7f383108d03a
kel orz <86697562+ellkrad@users.noreply.github.com>
2022-06-22 06:44:53 +0000

    変更

--- create.js
+++ create.js
@@ -1,20 +1,20 @@
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('test2.db');
 
-//let schema = `
-//create table test(
-//  id integer primary key,
-//  name text
-//);
-//`
-
 let schema = `
-create table car(
-id integer primary key,
-name text not null,
-maker_id integer not null
+create table test(
+  id integer primary key,
+ name text
 );
 `
+
+//let schema = `
+//`create table car(
+//`id integer primary key,
+//`name text not null,
+//`maker_id integer not null
+//`);
+//``
 
 db.serialize( () => {
 	db.run( schema, (error, row) => {
